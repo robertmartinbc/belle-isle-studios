e33b961f0614e2e29d2e36f8ed54444a3b487c1d
Robert J Martin <robertmartinbc@shaw.ca>
2013-08-11 22:26:35 -0700

    First commit and README.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BelleIsleStudios::Application.config.secret_token = 'b3d7fca54e2f700714d83841f657d05fd3b02cd5c99795bc4287f39e313809125c195ef187d3b8cb849ce8431b3cc54669b877c529ed423e37850fdce9a653b9'
