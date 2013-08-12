3d059d6b6ae91bf2d49359ac49bbc59709e8c048
Robert J Martin <robertmartinbc@shaw.ca>
2013-08-11 22:35:17 -0700

    Created welcome controller and associated views.

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,34 @@
+class WelcomeController < ApplicationController
+  def index
+  end
+
+  def biography
+  end
+
+  def studio
+  end
+
+  def instruments
+  end
+
+  def gear
+  end
+
+  def artists
+  end
+
+  def musician
+  end
+
+  def mascots
+  end
+
+  def workshops
+  end
+
+  def retreat
+  end
+
+  def contact
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,58 +1,25 @@
 BelleIsleStudios::Application.routes.draw do
-  # The priority is based upon order of creation:
-  # first created -> highest priority.
+  get "welcome/index"
 
-  # Sample of regular route:
-  #   match 'products/:id' => 'catalog#view'
-  # Keep in mind you can assign values other than :controller and :action
+  get "welcome/biography"
 
-  # Sample of named route:
-  #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
-  # This route can be invoked with purchase_url(:id => product.id)
+  get "welcome/studio"
 
-  # Sample resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
+  get "welcome/instruments"
 
-  # Sample resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
+  get "welcome/gear"
 
-  # Sample resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
+  get "welcome/artists"
 
-  # Sample resource route with more complex sub-resources
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', :on => :collection
-  #     end
-  #   end
+  get "welcome/musician"
 
-  # Sample resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
+  get "welcome/mascots"
 
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  get "welcome/workshops"
 
-  # See how all your routes lay out with "rake routes"
+  get "welcome/retreat"
 
-  # This is a legacy wild controller route that's not recommended for RESTful applications.
-  # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id))(.:format)'
+  get "welcome/contact"
+
+  root to: 'welcome#index'
 end
