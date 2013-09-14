ca42207788e7df0bc4432f58a0dd929e01b90f63
Robert Martin <robertmartinbc@shaw.ca>
2013-09-13 23:47:07 -0700

    Added photos of kitchen and studio.

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,5 +1,5 @@
 if Rails.env.development?
-  ActionMailer::Base.delivery_method = :sendmail
+  ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
