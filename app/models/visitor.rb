643650ded422a874a2169e15cb1c6da75df2710e
Robert Martin <robertmartinbc@shaw.ca>
2013-09-25 14:37:00 -0700

    Added validation to form.

--- app/models/visitor.rb
+++ app/models/visitor.rb
@@ -1,3 +1,10 @@
 class Visitor < ActiveRecord::Base
   attr_accessible :email, :end_date, :enquiry, :name, :start_date, :tele
+
+  validates :name, presence: true
+  validates :email, presence: true
+  validates :tele, presence: true
+  validates :start_date, presence: true
+  validates :end_date, presence: true
+  validates :enquiry, presence: true
 end
